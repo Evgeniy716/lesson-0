508352087638f8b2cb17ecf0b5c2dda70286e99d
Evgeniy716 <zenkavolk8448@gmail.com>
2024-06-06 10:27:39 +0300

    Add files via upload

---
+++ main2.py
@@ -0,0 +1,20 @@
+a = 'abracadabra'
+print(len(a))    #Задача 'Длина слова'.
+first = 5
+second = 10
+summa = first + second
+diff = first - second
+print(summa, diff)   #Задача 'Суммы и разности'.
+first = 3
+second = 5
+thing = 5
+means = first+second+thing/3
+print( means )          #Задача 'Среднеее арифметическое'.
+first_string = "Вторник"
+second_string = "Понедельник,"
+print(second_string, first_string)  #Задача 'Простые строчки'.
+a = 3
+b = 5
+c = 6
+f = (a*b) + (a*c)
+print(f**3/2)                     #Задача 'Сложная формула'.
