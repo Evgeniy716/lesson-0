a3d60b3b70103e03326e82f1efb3fc99f74583df
Evgeniy716 <zenkavolk8448@gmail.com>
2024-06-11 13:52:08 +0300

    Add files via upload

---
+++ Metod strok.py
@@ -0,0 +1,9 @@
+#name = input('Введите Ваше имя: ')
+#current_year = 2024
+#date_of_birth =int(input("в каком году вы родились ?"))
+#age = current_year - date_of_birth
+#print("Здравствуйте," , name)
+#print("в этом году Вам",age, 'лет')
+
+print('Привет , я строка в нижнем регистре'.upper())
+print('Привет , я строка в нижнем регистре'.replace("Привет",':Жопа'))
