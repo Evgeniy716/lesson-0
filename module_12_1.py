87288df29f09d5619dc15685c1835942b98bd3ca
Evgeniy716 <zenkavolk8448@gmail.com>
2024-10-17 09:58:37 +0300

    Add files via upload

--- module_12_1.py
+++ module_12_1.py
@@ -7,12 +7,12 @@ class RunnerTest(TestCase):
         r1 = Runner("Вася")
         for i in range(10):
             r1.walk()
-            self.assertEqual(r1.distance,50,f"{r1.name} прошел {r1.distance} а должен был 50")
+        self.assertEqual(r1.distance,50,f"{r1.name} прошел {r1.distance} а должен был 50")
     def test_run(self):
         r2 = Runner("Коля")
         for i in range(10):
             r2.run()
-            self.assertEqual(r2.distance, 100, f"{r2.name} прошел {r2.distance} а должен был 100")
+        self.assertEqual(r2.distance, 100, f"{r2.name} прошел {r2.distance} а должен был 100")
 
     def test_challenge(self):
         r1 = Runner("Вася")
